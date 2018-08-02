4cc73e56efa5d8381338707be5eb8d306e1b4c59
JCohner <joshuacohen2019@u.northwestern.edu>
2018-08-02 11:56:46 -0700

    simplified, just a keypad input i2c writer. all interpt done on main nano

--- Keypad_Input/Keypad_Input.ino
+++ Keypad_Input/Keypad_Input.ino
@@ -1,8 +1,6 @@
 #include <PinChangeInt.h>
-//#include <SoftwareSerial.h>
 #include <Keypad.h>
 #include <Wire.h>
-#include <TimerOne.h>
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -29,19 +27,8 @@ void setup(){
   pinMode(10,INPUT);
   pinMode(9,INPUT);
   pinMode(8,INPUT);
-  /*
-  //STATE INPUT (READY TO RECEIVE WIRE_LENGTH)
-  pinMode(13,INPUT);
-  //state change pin init/
-  pinMode(13, INPUT);
-  attachPinChangeInterrupt(13, state_change,CHANGE);
-  */
-  Wire.begin();
 
-  
-  
-  //Timer1.initialize(100);
-  //Timer1.attachInterrupt(sendData);
+  Wire.begin();
 }
 
 char stateKey;
@@ -60,48 +47,8 @@ void loop(){
         Wire.beginTransmission(8);
         Wire.write(stateKey);
         Wire.endTransmission(1);
-        /*If A input switch to numeric input state*/
-        if (stateKey == 'A'){
-          Serial.println("Swithcing to num input");
-          state = '1';
-          stateKey_prev = stateKey;
-        }
-      }
-      /*
-      } else {
-        stateKey = stateKey_prev;
-      }
-      Serial.println("transmitting A");
-      */
-      break;
-    /*Numeric input case*/  
-    case '1':
-      if (stateKey){
-        Serial.print("Numkey: ");
-        Serial.println(stateKey);
-        Wire.beginTransmission(8);
-        Wire.write(stateKey);
-        Wire.endTransmission(1);
-        /*If D input switch back to state input case*/
-        if (stateKey == 'D'){
-          state = '0';
-        }
       }
       break;
   }
 }
-
-/*
-bool state_change_state = 1;
-void state_change() {
-  if(state_change_state){
-    state = '1';
-    state_change_state = !state_change_state;
-  } else {
-    state = '0';
-    state_change_state = !state_change_state;
-  }
- 
-}
-*/
 
