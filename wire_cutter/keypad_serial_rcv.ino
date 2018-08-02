149a8f54b4bf229e845d902a62dd196a40031419
JCohner <joshuacohen2019@u.northwestern.edu>
2018-08-02 11:57:38 -0700

    implemented error checking on user inputs

--- wire_cutter/keypad_serial_rcv.ino
+++ wire_cutter/keypad_serial_rcv.ino
@@ -1,6 +1,12 @@
+#include <Wire.h>
+#include <TimerOne.h>
+#include <string.h>
 
 void keypad_serial_setup(void){
   Serial.begin(9600);
+  //pinMode(13,OUTPUT);
+  Wire.begin(8); //join i2c bus as slave
+  Wire.onReceive(get_keypad_state);
 }
 void print_incoming_byte(void){
   /* CODE FOR NUM PAD INTERFACE FROM SECOND NANO*/
@@ -11,5 +17,45 @@ void print_incoming_byte(void){
       //Serial.println(i);
       lcd.print(incomingByte);
     }
+}
+
+char c;
+void get_keypad_state(int howMany){
+  if (machine_state == 'A'){
+    /*
+     * Special case in which we want user input of how many feet to cut
+    */
+    while (Wire.available()) {
+      c = Wire.read();
+    }
+    if(c == 'D'){
+      machine_state = 'D';
+      if (strlen(A_prompt_buffer) > 5){
+        machine_state = 'E';
+        E_flag = 2;
+      }
+      wire_length = atoi(A_prompt_buffer);
+      if ((wire_length > 32757) || (wire_length < 0)){
+        machine_state = 'E';
+        E_flag = 1;
+      }
+    }
+    A_prompt_buffer[A_prompt_buffer_indexer++] = c;
+    lcd.print(c);
+  } else {
+    /*
+     * Normal operation, switches between states of the state machine
+     * i.e. "load spool" - > "how many feet" -> "operating" -> etc
+    */
+    while (Wire.available()) {
+      c = Wire.read();
+    }
+    machine_state = c;
+  }
+  
+}
+
+char get_keypad_input(){
+  return c;
 }
 
