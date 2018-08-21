13c9699251950fc03febe17f789fec95c52755a4
JCohner <joshuacohen2019@u.northwestern.edu>
2018-08-21 15:11:50 -0700

    added get_spool_dist which translates encoder counts to spooled length (6 in diam measuring wheel)

--- wire_cutter/enocder.ino
+++ wire_cutter/enocder.ino
@@ -1,5 +1,8 @@
 #define Interrupt_A 2 //D2
 #define Interrupt_B 3 //D3
+#define WHEEL_DIAM 6 //in inches 
+#include <stdio.h>
+float INCHES_PER_COUNT = 0.0117809725;//6.0 * 3.14159 / 1600.0; 
 
 volatile unsigned long int CW_count = 0;
 volatile unsigned long int CCW_count = 0;
@@ -13,7 +16,7 @@ void encoder_setup(){
   pinMode(Interrupt_A, INPUT);
   pinMode(Interrupt_B, INPUT);
   attachInterrupt(digitalPinToInterrupt(Interrupt_A), quadrature_count_A, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(Interrupt_B), quadrature_count_B, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Interrupt_B), quadrature_count_A, CHANGE);
 }
 
 void quadrature_count_A(){
@@ -49,7 +52,18 @@ unsigned long int get_CCW(void){
   return CCW_count;
 }
 
-unsigned long int get_spool_counts(void){
-  return (CW_count - CCW_count);
+long int get_spool_counts(void){
+  long int spool_counts = ((long int) CW_count) - ((long int)CCW_count);
+  return spool_counts; 
+}
+
+void zero_spool_counts(void){
+  CW_count = 0;
+  CCW_count = 0;
+  return;
+}
+
+double get_spool_dist(void){
+  return ((INCHES_PER_COUNT / 12.0) * ((double) get_spool_counts())); 
 }
 
