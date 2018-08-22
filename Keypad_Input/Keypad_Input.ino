ae7926af628efdfd8eb147f092477852d5dbca2b
JCohner <joshuacohen2019@u.northwestern.edu>
2018-08-22 14:46:12 -0700

    simple change of '*' char to '.' char for easier conversion to double

--- Keypad_Input/Keypad_Input.ino
+++ Keypad_Input/Keypad_Input.ino
@@ -9,7 +9,7 @@ char hexaKeys[ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'.','0','#','D'}
 };
 byte rowPins[ROWS] = {7, 6, 5, 4}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {11, 10, 9, 8}; //connect to the column pinouts of the keypad
